945bca3bab00f03f2141380437600ce4cc9dac44
sdwebb <swebb@radiasoft.net>
2017-02-13 17:18:52 -0700

    Fixed a bug in computing the particle energies. The drift motion is now second order accurate with zero initial fields.

--- rssympim/sympim_rz/data/particle_data.py
+++ rssympim/sympim_rz/data/particle_data.py
@@ -43,7 +43,7 @@ class particle_data:
                               field_data.compute_Ar(self.r,self.z))**2 +\
                              (self.pz - self.qOc*
                               field_data.compute_Az(self.r, self.z))**2 +\
-                             self.ell**2/(self.mc*self.r**2) +
+                             self.ell**2/(self.r**2) +
                              (self.mc)**2
                              )/(self.mc)
 
