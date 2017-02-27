1ad3ea036f0b5d196dfc838d0fba75a9dd3b907b
sdwebb <swebb@radiasoft.net>
2017-02-27 11:27:25 -0700

    Getting rid of libraries
    This was half-baked cruft.

--- rssympim/examples/rz_test.py
+++ rssympim/examples/rz_test.py
@@ -7,16 +7,18 @@ from matplotlib import pyplot as plt
 import time
 
 
-n_ptcls = 18000
-n_r = 30
-n_z = 60
+n_ptcls = 1000
+n_r = 10
+n_z = 10
 
 ptcl_data = particle_data.particle_data(n_ptcls, 1., 1.3, 33.5)
 fld_data  = field_data.field_data(5., 2., n_r, n_z)
 
+fld_data.mode_coords = np.ones((n_r, n_z, 2))
+
 ptcl_data.r  = np.ones(n_ptcls)
 for idx in range(0, n_ptcls):
-    ptcl_data.r[idx]*= idx*(4.)/n_ptcls + .01
+    ptcl_data.r[idx] *= idx*(4.)/n_ptcls + .01
 ptcl_data.pr = ptcl_data.mc*np.arange(0.,.5,.5/n_ptcls)
 ptcl_data.ell = ptcl_data.r*ptcl_data.pr*.1
 
@@ -31,13 +33,15 @@ particle_energies = ptcl_data.compute_ptcl_energy(fld_data)
 field_energies = fld_data.compute_energy()
 tot_energy = np.sum(particle_energies) + np.sum(field_energies)
 
+E0 = tot_energy
+
 E = []
 t = []
 
-E.append(tot_energy)
-t.append(0.)
+#E.append(tot_energy)
+#t.append(0.)
 
-n_steps = 10
+n_steps = 100
 step = 0
 
 t0 = time.time()
@@ -53,9 +57,9 @@ while step < n_steps:
     step += 1
     print 'simulation completed step', str(step)
 
-    tot_energy = np.sum(particle_energies) + np.sum(field_energies)
+#    tot_energy = np.sum(particle_energies) + np.sum(field_energies)
 
-    E.append(tot_energy)
+    E.append(abs(tot_energy-E0)/E0)
     t.append(step * dt)
 
 tf = time.time()
