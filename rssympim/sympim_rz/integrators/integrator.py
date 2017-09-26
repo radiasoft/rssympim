c6acd26499e7c8b97f13505c6d29c0d307857666
ncook882 <ncook@radiasoft.net>
2017-09-25 18:42:21 -0600

    Created a complete second-order single step function

--- rssympim/sympim_rz/integrators/integrator.py
+++ rssympim/sympim_rz/integrators/integrator.py
@@ -50,3 +50,37 @@ class integrator:
     def half_field_back(self, field_data):
 
         self.field_maps.half_advance_back(field_data)
+        
+    def second_order_step(self, ptcl_data, field_data):
+        '''A comlpete step with second order integration in time'''
+        
+        # always compute the new gamma_mc after the field map update
+        ptcl_data.compute_gamma_mc(field_data)
+
+        # Update sequence goes
+        # M_ell S_r D_r S_r^-1 S_z D_z S_z^-1 S_r D_r S_r^-1 M_ell
+        self.ptcl_maps.half_angular_momentum(ptcl_data)
+
+        self.sim_maps.S_r(field_data, ptcl_data)
+        self.ptcl_maps.half_drift_r(ptcl_data, field_data)
+        self.sim_maps.S_r_inverse(field_data, ptcl_data)
+
+        self.sim_maps.S_z(field_data, ptcl_data)
+        self.ptcl_maps.drift_z(ptcl_data)
+        self.sim_maps.S_z_inverse(field_data, ptcl_data)
+
+        self.sim_maps.S_r(field_data, ptcl_data)
+        self.ptcl_maps.half_drift_r(ptcl_data, field_data)
+        self.sim_maps.S_r_inverse(field_data, ptcl_data)
+
+        self.ptcl_maps.half_angular_momentum(ptcl_data)
+
+        # Add the delta-P to each mode
+        field_data.finalize_fields()
+        
+        #full step particle
+        self.single_step_ptcl(ptcl_data, field_data)
+        
+        #half step field
+        self.field_maps.half_advance_forward(field_data)
+        
