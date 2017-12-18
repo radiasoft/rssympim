c4ae6f7688e44f12f0cd642148454679c6b5a4ce
Stephen Webb <swebb@Stephens-MacBook-Pro.local>
2017-12-18 15:25:11 -0700

    getting close to its final form

--- rssympim/examples/wake_dynamics/wake_formation.py
+++ rssympim/examples/wake_dynamics/wake_formation.py
@@ -36,6 +36,8 @@ import numpy as np
 
 from rssympim.sympim_rz.io import field_io, particle_io
 
+from mpi4py import MPI as mpi
+
 ###
 #
 # Specify the bunch parameters. Assumes a parabolic distribution
@@ -78,6 +80,8 @@ k_p = np.sqrt(4*np.pi*n0 *
                   consts.electron_charge*consts.electron_charge /
                   (consts.electron_mass*consts.c))
 
+plasma_temperature = 1000.*consts.k_boltzmann
+
 # We are simulating electrons
 charge = consts.electron_charge
 mass = consts.electron_mass
@@ -88,7 +92,7 @@ mass = consts.electron_mass
 
 # Run time considerations
 
-simulation_time = 100 # 2.*np.pi/k_p, in units of plasma oscillations
+beta_beam = 1. # beam v_z/speed of light
 domain_r = 4 # 2.*np.pi/k_p
 domain_l = 5 # 2.*np.pi/k_p
 steps_per_plasma_period = 30
@@ -102,6 +106,12 @@ num_macro_per_mode = 10
 # Do not modify below this line
 #
 #--------------------------------------------------------------------
+
+# MPI stuff
+
+comm = mpi.COMM_WORLD
+size = comm.size
+rank = comm.rank
 
 # Beam parameters
 
@@ -129,46 +139,91 @@ n_macro_ptcls = num_macro_per_mode*n_modes_z*n_modes_r
 n_electrons = np.pi*radius*radius*length*n0
 macro_weight = n_electrons/n_macro_ptcls
 
+# particle per core
+n_ptcls_per_core = n_macro_ptcls/size
+
+# This leaves a remainder, which we distribute evenly
+n_left = n_macro_ptcls%size
+if rank < n_left:
+    n_ptcls_per_core += 1
+
 fld_data = field_data.field_data(length, radius,
                                  n_modes_z, n_modes_r)
-ptcl_data = particle_data.particle_data(n_macro_ptcls, charge, mass, macro_weight)
+ptcl_data = particle_data.particle_data(n_ptcls_per_core,
+                                        charge, mass, macro_weight)
 
 # Initial conditions
-for ptcl_idx in range(0, n_macro_ptcls):
-    # distribute the particles on a grid, gonna have to use linear stride here
+
+# uniform particle distribution in x, y, z
+x = 2.*radius*np.random.rand(n_ptcls_per_core)-radius
+y = 2.*radius*np.random.rand(n_ptcls_per_core)-radius
+r = np.sqrt(x*x + y*y)
+z = length*np.random.rand(n_ptcls_per_core)
+
+# Use colon notation to highlight bugs in counting
+ptcl_data.r[:] = r[:]
+ptcl_data.z[:] = z[:]
 
 macro_volume = np.sum(ptcl_data.r)
 # Properly adjust the weights for constant density
-for ptcl_idx in range(0, n_macro_ptcls):
-    ptcl_data.qOc[ptcl_idx] *= n_macro_ptcls*ptcl_data.r[ptcl_idx]/macro_volume
-    ptcl_data.q[ptcl_idx]   *= n_macro_ptcls*ptcl_data.r[ptcl_idx]/macro_volume
-    ptcl_data.m[ptcl_idx]   *= n_macro_ptcls*ptcl_data.r[ptcl_idx]/macro_volume
-    ptcl_data.mc[ptcl_idx]  *= n_macro_ptcls*ptcl_data.r[ptcl_idx]/macro_volume
+scale_factor = n_ptcls_per_core*ptcl_data.r/macro_volume
 
+ptcl_data.qOc *= scale_factor
+ptcl_data.q   *= scale_factor
+ptcl_data.m   *= scale_factor
+ptcl_data.mc  *= scale_factor
+
+# Generate a non-relativistic thermal distribution
+v_x = np.random.normal(0., plasma_temperature/(2*consts.electron_mass), n_ptcls_per_core)
+v_y = np.random.normal(0., plasma_temperature/(2*consts.electron_mass), n_ptcls_per_core)
+v_z = np.random.normal(0., plasma_temperature/(2*consts.electron_mass), n_ptcls_per_core)
+
+# pretend the r-axis is aligned to the x-axis for simplicity
+ell = x*v_y
+v_r = v_x
+
+ptcl_data.ell = ell * ptcl_data.m
+ptcl_data.pz  = v_z * ptcl_data.m
+ptcl_data.pr  = v_r * ptcl_data.m
 
 #
 # Stimulate the beam exciting the wake, then dump
 #
 
-sim_len = 2.*z0 + length
+sim_len = 2.*z_beam + length
 nsteps = int(sim_len/dtau)
+
+if rank == 0:
+    print 'Simulation parameters:'
+    print '|---------------------'
+    print ' domain length', length, 'cm'
+    print ' domain radius', radius, 'cm'
+    print ' k_p          ', k_p, 'cm^-1'
+    print ' dtau         ', dtau, 'cm'
+    print ' nsteps       ', nsteps
+    print ' macroptcls   ', n_macro_ptcls
+    print ' ptcls/core   ', n_ptcls_per_core
 
 step_num = 0
 
 # Start the beam off the simulation domain
 beam_pos = -2.*z_beam + step_num * dtau
 
-modfied_ptcl_update_sequence = \
+modified_ptcl_update_sequence = \
     beam_integrator.beam_integrator(r_beam, z_beam, n_beam, dtau, fld_data)
 
 while beam_pos < length:
 
     # add field maps
 
-    modified_ptcl_update_sequence.update(ptcl_data, fld_data, beam_pos, 0.5*dtau)
+    modified_ptcl_update_sequence.update(ptcl_data, fld_data, beam_pos)
 
     # Beam is moving at the speed of light
-    beam_pos += dtau
+    beam_pos += beta_beam*dtau
+
+    if rank == 0:
+        if step_num%100 == 0:
+            print 'completing step', step_num
 
     step_num += 1
 
