2ac912f341253d0fe55cfd0279999b38ed77ffc9
Stephen Webb <swebb@Stephens-MacBook-Pro.local>
2017-12-18 14:44:30 -0700

    Added the boltzmann constant in cgs

--- rssympim/constants/constants.py
+++ rssympim/constants/constants.py
@@ -1,6 +1,12 @@
 # Set of constants for common use across SymPIM
 
+# Speed of light
 c = 29979245800  # centimeters/second
+
+# Fundamental particle quantities
 electron_charge = 4.80325e-10 #esu
-electron_mass = 9.10938e-28 #grams
-proton_mass = 1.676219e-24 #grams
+electron_mass = 9.10938e-28   #grams
+proton_mass = 1.676219e-24    #grams
+
+# Thermodynamic quantities
+k_boltzmann = 1.3807e-16      #ergs/Kelvin
