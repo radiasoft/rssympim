db1fb9c041af899b3c0f2c91581efefb2e16d295
swebb <swebb@radiasoft.net>
2018-03-26 11:30:22 -0600

    Fixing longitudinal_thermal
    to do nothing if no new particles are required.

--- rssympim/sympim_rz/boundaries/longitudinal_thermal.py
+++ rssympim/sympim_rz/boundaries/longitudinal_thermal.py
@@ -38,49 +38,52 @@ class longitudinal_thermal:
         out_of_bounds_forward = np.where(ptcl_data.z > fld_data.domain_L)
 
         n_new_ptcls = np.shape(out_of_bounds_forward)[0]
+        if n_new_ptcls > 0:
 
-        # pretend all the particles are on the x-axis for simplicity
-        sigma = np.sqrt(consts.k_boltzmann*self.temp/consts.electron_mass)
-        vx  = np.random.normal(0.,sigma, n_new_ptcls)
-        vy  = np.random.normal(0.,sigma, n_new_ptcls)
-        vz  = -np.abs(np.random.normal(0.,sigma, n_new_ptcls)) # Must be moving back into domain
-        ptcl_data.z[out_of_bounds_forward] = fld_data.domain_L
+            # pretend all the particles are on the x-axis for simplicity
+            sigma = np.sqrt(consts.k_boltzmann*self.temp/consts.electron_mass)
+            vx  = np.random.normal(0.,sigma, n_new_ptcls)
+            vy  = np.random.normal(0.,sigma, n_new_ptcls)
+            vz  = -np.abs(np.random.normal(0.,sigma, n_new_ptcls)) # Must be moving back into domain
+            ptcl_data.z[out_of_bounds_forward] = fld_data.domain_L
 
-        Ar = fld_data.compute_Ar(ptcl_data.r[out_of_bounds_forward],
-                                 ptcl_data.z[out_of_bounds_forward],
-                                 ptcl_data.qOc[out_of_bounds_forward])
-        Az = fld_data.compute_Az(ptcl_data.r[out_of_bounds_forward],
-                                 ptcl_data.z[out_of_bounds_forward],
-                                 ptcl_data.qOc[out_of_bounds_forward])
+            Ar = fld_data.compute_Ar(ptcl_data.r[out_of_bounds_forward],
+                                     ptcl_data.z[out_of_bounds_forward],
+                                     ptcl_data.qOc[out_of_bounds_forward])
+            Az = fld_data.compute_Az(ptcl_data.r[out_of_bounds_forward],
+                                     ptcl_data.z[out_of_bounds_forward],
+                                     ptcl_data.qOc[out_of_bounds_forward])
 
-        weighted_mass = ptcl_data.mass*ptcl_data.weight[out_of_bounds_forward]
+            weighted_mass = ptcl_data.mass*ptcl_data.weight[out_of_bounds_forward]
 
-        ptcl_data.pr[out_of_bounds_forward] = weighted_mass*vx + Ar
-        ptcl_data.pz[out_of_bounds_forward] = weighted_mass*vz + Az
-        # set the angular momentum
-        ptcl_data.ell[out_of_bounds_forward] = weighted_mass*vy*ptcl_data.r[out_of_bounds_forward]
+            ptcl_data.pr[out_of_bounds_forward] = weighted_mass*vx + Ar
+            ptcl_data.pz[out_of_bounds_forward] = weighted_mass*vz + Az
+            # set the angular momentum
+            ptcl_data.ell[out_of_bounds_forward] = weighted_mass*vy*ptcl_data.r[out_of_bounds_forward]
 
         out_of_bounds_backward = np.where(ptcl_data.z < 0.)
 
         n_new_ptcls = np.shape(out_of_bounds_backward)[0]
 
-        # pretend all the particles are on the x-axis for simplicity
-        sigma = np.sqrt(consts.k_boltzmann*self.temp/consts.electron_mass)
-        vx  = np.random.normal(0.,sigma, n_new_ptcls)
-        vy  = np.random.normal(0.,sigma, n_new_ptcls)
-        vz  = -np.abs(np.random.normal(0.,sigma, n_new_ptcls)) # Must be moving back into domain
-        ptcl_data.z[out_of_bounds_backward] = 0.
+        if n_new_ptcls > 0:
 
-        Ar = fld_data.compute_Ar(ptcl_data.r[out_of_bounds_backward],
-                                 ptcl_data.z[out_of_bounds_backward],
-                                 ptcl_data.qOc[out_of_bounds_backward])
-        Az = fld_data.compute_Az(ptcl_data.r[out_of_bounds_backward],
-                                 ptcl_data.z[out_of_bounds_backward],
-                                 ptcl_data.qOc[out_of_bounds_backward])
+            # pretend all the particles are on the x-axis for simplicity
+            sigma = np.sqrt(consts.k_boltzmann*self.temp/consts.electron_mass)
+            vx  = np.random.normal(0.,sigma, n_new_ptcls)
+            vy  = np.random.normal(0.,sigma, n_new_ptcls)
+            vz  = -np.abs(np.random.normal(0.,sigma, n_new_ptcls)) # Must be moving back into domain
+            ptcl_data.z[out_of_bounds_backward] = 0.
 
-        weighted_mass = ptcl_data.mass*ptcl_data.weight[out_of_bounds_backward]
+            Ar = fld_data.compute_Ar(ptcl_data.r[out_of_bounds_backward],
+                                     ptcl_data.z[out_of_bounds_backward],
+                                     ptcl_data.qOc[out_of_bounds_backward])
+            Az = fld_data.compute_Az(ptcl_data.r[out_of_bounds_backward],
+                                     ptcl_data.z[out_of_bounds_backward],
+                                     ptcl_data.qOc[out_of_bounds_backward])
 
-        ptcl_data.pr[out_of_bounds_backward] = weighted_mass*vx + Ar
-        ptcl_data.pz[out_of_bounds_backward] = weighted_mass*vz + Az
-        # set the angular momentum
-        ptcl_data.ell[out_of_bounds_backward] = weighted_mass*vy*ptcl_data.r[out_of_bounds_backward]
+            weighted_mass = ptcl_data.mass*ptcl_data.weight[out_of_bounds_backward]
+
+            ptcl_data.pr[out_of_bounds_backward] = weighted_mass*vx + Ar
+            ptcl_data.pz[out_of_bounds_backward] = weighted_mass*vz + Az
+            # set the angular momentum
+            ptcl_data.ell[out_of_bounds_backward] = weighted_mass*vy*ptcl_data.r[out_of_bounds_backward]
